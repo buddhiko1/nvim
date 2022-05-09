124ec507bc58887187c8f053642d67b0ae0e1f34
dhammena <dhammena@outlook.com>
2022-05-09 18:13:59 +0800

    options : merge

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,58 +1,93 @@
 local opt = vim.opt
 local g = vim.g
 
--- use filetype.lua instead of filetype.vim
-g.did_load_filetypes = 0
-g.do_filetype_lua = 1
-g.toggle_theme_icon = "   "
-
-opt.confirm = true
-opt.laststatus = 3 -- global statusline
-opt.title = true
-opt.clipboard = "unnamedplus"
-opt.cmdheight = 1
-opt.cul = true -- cursor line
-
--- Indentline
-opt.expandtab = true
-opt.shiftwidth = 2
-opt.smartindent = true
-
--- disable tilde on end of buffer: https://github.com/neovim/neovim/pull/8546#issuecomment-643643758
-opt.fillchars = { eob = " " }
-
-opt.hidden = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.mouse = "a"
-
--- Numbers
-opt.number = true
-opt.numberwidth = 2
-opt.relativenumber = false
-opt.ruler = false
-
--- disable nvim intro
-opt.shortmess:append "sI"
-
-opt.signcolumn = "yes"
-opt.splitbelow = true
-opt.splitright = true
-opt.tabstop = 8
-opt.termguicolors = true
-opt.timeoutlen = 400
-opt.undofile = true
-
--- interval for writing swap file to disk, also used by gitsigns
-opt.updatetime = 250
-
--- go to previous/next line with h,l,left arrow and right arrow
--- when cursor reaches end/beginning of line
-opt.whichwrap:append "<>[]hl"
 g.mapleader = " "
 
--- disable some builtin vim plugins
 
+-- filetype and encode
+g.did_load_filetypes = 0
+g.do_filetype_lua = 1 -- use filetype.lua instead of filetype.vim
+
+g.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+
+-- behavior
+opt.confirm = true
+
+opt.autoread = true
+
+opt.hidden = true
+
+opt.updatetime = 300
+opt.timeoutlen = 350
+
+opt.wrap = true
+opt.tw = 0
+opt.whichwrap = '<,>,[,]' -- go to previous/next line
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+
+opt.shiftround = true
+opt.shiftwidth = 2
+opt.autoindent = true
+
+opt.undofile = true
+
+
+-- display
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.number = true
+opt.relativenumber = true
+
+opt.cul = true -- cursor line
+
+opt.showmode = false
+
+opt.signcolumn = "yes:1" -- with 1 fixed space to show 2 icon
+
+opt.showcmd = true
+opt.cmdheight = 1
+
+opt.splitbelow = true
+opt.splitright = true
+
+opt.list = true -- show space
+opt.listchars = "tab:> ,trail:·"
+
+opt.laststatus = 3 -- global statusline
+
+opt.title = true
+
+g.toggle_theme_icon = "   "
+
+opt.fillchars = { eob = " " } -- disable tilde on end of buffer: https://github.com/neovim/neovim/pull/8546#issuecomment-643643758
+
+
+-- search
+opt.smartcase = true
+opt.hlsearch = false
+opt.incsearch = true
+
+
+-- function
+opt.wildmenu = true -- auto completion on command
+g.completeopt = "menu,menuone,noselect,noinsert"
+
+opt.viewoptions = "cursor,folds,slash,unix" -- remember where to recover cursor
+
+opt.mouse = "a"
+
+opt.backup = false
+opt.writebackup = false
+opt.swapfile = false
+
+
+-- disable some builtin vim plugins
 local default_plugins = {
    "2html_plugin",
    "getscript",
@@ -73,15 +108,6 @@ local default_plugins = {
    "zip",
    "zipPlugin",
 }
-
 for _, plugin in pairs(default_plugins) do
    g["loaded_" .. plugin] = 1
 end
-
-vim.schedule(function()
-   vim.opt.shadafile = vim.fn.expand "$HOME" .. "/.local/share/nvim/shada/main.shada"
-   vim.cmd [[ silent! rsh ]]
-end)
-
--- load user options if the file exists
-require("core.utils").load_config().options.user()
