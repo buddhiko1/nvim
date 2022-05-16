edc0e1aa2d8d1425cd4028c8cfe134055ceae7ff
dhammena <dhammena@outlook.com>
2022-05-16 14:31:04 +0800

    amend

---
+++ lua/lsp/common.lua
@@ -0,0 +1,31 @@
+local map = require("utils").map
+
+local M = {}
+
+M.on_attach = function(client, bufnr)
+  local opt = { buffer = bufnr }
+  map("n", "<leader>do", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
+  map("n", "<leader>dp", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
+  map("n", "<leader>dn", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
+  map("n", "<leader>ds", "<cmd>lua vim.diagnostic.setloclist()<CR>", opt)
+
+  map("n", "<leader>sv", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
+  map("n", "<leader>sc", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
+  map("n", "<leader>sd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
+  map("n", "<leader>sr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
+  map("n", "<leader>st", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opt)
+  map("n", "<leader>sh", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opt)
+
+  map("n", "<leader>ji", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
+  map("n", "<leader>jr", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
+  map("n", "<leader>ja", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
+  map("n", "<leader>jf", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
+
+  map("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opt)
+  map("n", "<leader>wd", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opt)
+  map("n", "<leader>wl", "<cmd>lua vim.lsp.buf.list_workspace_folders()<CR>", opt)
+end
+
+M.debounce_text_changes = 150
+
+return M
--- lua/plugins/configs/lsp_config.lua
+++ lua/plugins/configs/lsp_config.lua
@@ -1,27 +1,4 @@
 local load = require("utils").load
-local map = require("utils").map
-
-local on_attach = function(client, bufnr)
-  local opt = { buffer = bufnr }
-  map("n", "<leader>f", "<cmd>lua vim.diagnostic.open_float()<CR>", opt)
-  map("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opt)
-  map("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opt)
-  map("n", "<leader>s", "<cmd>lua vim.diagnostic.setloclist()<CR>", opt)
-
-  map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opt)
-  map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opt)
-  map("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opt)
-  map("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opt)
-  map("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opt)
-  map("n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opt)
-  map("n", "<leader>ra", "<cmd>lua vim.lsp.buf.rename()<CR>", opt)
-  map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opt)
-  map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opt)
-  map("n", "<leader>fm", "<cmd>lua vim.lsp.buf.formatting()<CR>", opt)
-  map("n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opt)
-  map("n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opt)
-  map("n", "<leader>wl", "<cmd>lua vim.lsp.buf.list_workspace_folders()<CR>", opt)
-end
 
 local amend = function()
   vim.diagnostic.config({
@@ -43,12 +20,13 @@ local M = {}
 M.config = function ()
   amend()
   local lspconfig = load("lspconfig")
+  local common_opts = load("lsp.common")
   local servers = { 'cssls','html','tsserver','graphql','jsonls','sqlls','sumneko_lua','dockerls','yamlls','remark_ls' }
   for _, lsp in pairs(servers) do
     lspconfig[lsp].setup {
-      on_attach = on_attach,
+      on_attach = common_opts.on_attach,
       flags = {
-        debounce_text_changes = 150,
+        debounce_text_changes = common_opts.debounce_text_changes
       }
     }
   end
