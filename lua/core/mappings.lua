68a29f94f07771b0cc6774f1d03ad2b7bdb6564c
dhammena <dhammena@outlook.com>
2022-05-18 16:21:10 +0800

    remapping

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -34,12 +34,12 @@ map("i", "<C-j>", "<ESC>1ji")
 map("i", "<C-k>", "<ESC>1ki")
 map("i", "<C-b>", "<Home>")
 map("i", "<C-e>", "<End>")
-map("i", "<C-f>", "<ESC>wi")
-map("i", "<C-b>", "<ESC>bi")
+map("i", "<C-h>", "<ESC>hi")
+map("i", "<C-l>", "<ESC>li")
 
 
 -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
--- http<cmd> ://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
+-- http://www.reddit.com/r/vim/comments/2k4cbr/problem_with_gj_and_gk/
 -- empty mode is same as using <cmd> :map
 -- also don't use g[j|k] when in operator pending mode, so it doesn't alter d, y or c behaviour
 map({ "n", "x", "o" }, "j", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
@@ -47,22 +47,14 @@ map({ "n", "x", "o" }, "k", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { ex
 map("", "<Down>", 'v:count || mode(1)[0:1] == "no" ? "j" : "gj"', { expr = true })
 map("", "<Up>", 'v:count || mode(1)[0:1] == "no" ? "k" : "gk"', { expr = true })
 
--- behavior
-map("n", "<Esc>", "<cmd> :noh <CR>") -- shut down the search high light
-map("n", "<leader>q", "q") -- recording
--- map("n", "q", "<cmd> :q! <CR>") -- recording
-map("n", "<leader>j", "J") -- join
-
--- undo redo
--- map("n", "<C-z>", "u", opt("Undo"))
--- map("n", "<C-Z>", "<C-r>", opt("Redo"))
--- map("i", "<C-z>", "<ESC>u", opt("Undo"))
--- map("i", "<C-Z>", "<ESC><C-r>", opt("Redo"))
-
--- copy
+-- copy and past
 map("v", "<C-y>", [["+y]])
-map("n", "<C-v>", [[i<End><CR><ESC>"+p]])
+map("n", "<C-v>", [["+p]])
 map("i", "<C-v>", [[<ESC>"+pa]])
+
+-- assistant
+map("n", "pp", [[i<End><CR><ESC>pi<End><CR><ESC>]])
+map("n", "<leader><leader>", [[i<End><CR><ESC>]])
 
 -- statusbar toggle
 map("n", "<leader>y", "<cmd> :set laststatus=0 <CR>")
@@ -70,3 +62,9 @@ map("n", "<leader>yy", "<cmd> :set laststatus=3 <CR>")
 
 -- packer
 map("n", "<leader>uu", "<cmd> :PackerSync <CR>")
+
+-- remapping
+map("n", "<Esc>", "<cmd> :noh <CR>") -- shut down the search high light
+map("n", "<leader>q", "q") -- recording
+map("n", "<leader>j", "J") -- join
+map("n", "<A-v>", "<C-v>")
