f501be289363c0bc81825f55b304e8c3c003e036
dhammena <dhammena@outlook.com>
2022-08-15 15:26:09 +0800

    'win10'

--- lua/plugins/configs/nightfox.lua
+++ lua/plugins/configs/nightfox.lua
@@ -1,22 +1,41 @@
 local load = require("utils").load
 local map = require("utils").map
+local switch_alacritty_theme = require("utils").switch_alacritty_theme
 
 local M = {}
 
+local _switch_theme = function(theme)
+  -- must switch alacritty theme frist for fixing fullscreen bug on windows 
+  if vim.loop.os_uname().sysname == "Windows_NT" then
+    switch_alacritty_theme(theme)
+  end
+
+  vim.cmd("colorscheme "..theme)
+
+  -- switch cursor theme
+  if theme == "duskfox" then
+    vim.cmd("highlight Cursor guibg=#cc0000 guifg=black")
+    vim.cmd("set guicursor=n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor")
+  else
+    vim.cmd("colorscheme dawnfox")
+    vim.cmd("highlight Cursor guibg=black guifg=white")
+    vim.cmd("set guicursor=n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20")
+  end
+end
+
 M.setup = function()
-  -- theme switch
+  -- keymap for toggle theme
   map("n", "<leader>t", function()
     local theme = vim.g.colors_name
     if theme == "dawnfox" then
-      vim.cmd("colorscheme duskfox")
-      vim.cmd("highlight Cursor guibg=#cc0000 guifg=black")
-      vim.cmd("set guicursor=n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor")
+      _switch_theme("duskfox")
     else
-      vim.cmd("colorscheme dawnfox")
-      vim.cmd("set guicursor=n-v-c-sm:block,i-ci-ve:ver25-Cursor,r-cr-o:hor20")
+      _switch_theme("dawnfox")
     end
-    require("plugins.configs.tabline").reload() -- sync tabline color
-    vim.cmd("set laststatus=0") --lualine bug fix
+    -- sync tabline color
+    require("plugins.configs.tabline").reload()
+    --lualine bug fix
+    vim.cmd("set laststatus=0")
   end)
 end
 
@@ -45,7 +64,9 @@ M.config = function()
   }
 
   nightfox.setup(options)
-  vim.cmd "colorscheme dawnfox"
+
+  -- init theme
+  _switch_theme("duskfox")
 end
 
 return M
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-## run package update when fresh install.
+## ArchLinux 
+- Run PackerSync when fresh install.
+
+## Windows
+1. Install alacritty.
+2. Install [alacritty-theme-switch]("https://github.com/tichopad/alacritty-theme-switch").
+3. Change alacritty_path's value appropriately in utils.lua.
+4. Change start path value appropriately in start.vbs.
+5. Send start.vbs to desktop.
+6. Create shortcut for start.vbs's shortcut.
