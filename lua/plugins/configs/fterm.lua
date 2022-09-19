fb6af836ae4a7975785dd7b0077f18cff14e3540
dhammena <dhammena@outlook.com>
2022-09-19 16:53:54 +0800

    amend

--- lua/plugins/configs/fterm.lua
+++ lua/plugins/configs/fterm.lua
@@ -25,7 +25,7 @@ M.config = function()
 
   local gitui = fterm:new({
     ft = 'gitui', -- file type
-    cmd = "/usr/bin/ssh-add 2>> /dev/null && gitui",
+    cmd = "g",
     dimensions = {
       height = 0.9,
       width = 0.9
