c2cb749d67456924da012b8fd3a6f085da345498
dhammena <dhammena@outlook.com>
2022-09-27 17:44:52 +0800

    fterm: amend

--- lua/plugins/configs/fterm.lua
+++ lua/plugins/configs/fterm.lua
@@ -7,6 +7,7 @@ local M = {}
 M.setup = function()
   map('n', '<leader>v', '<CMD>lua require("FTerm").toggle()<CR>')
   map('t', '<leader>v', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+  map('t', '<leader>q', '<C-\\><C-n><CMD>lua require("FTerm").exit()<CR>')
 end
 
 M.config = function()
